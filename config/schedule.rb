c47160505c2153f3aac7216cdbd4c4424dd18895
Michal Olah <olahmichal@gmail.com>
2011-02-04 12:42:54 +0100

    removed unneeded rails_env from crontab config and removed /public/system from git

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,7 +20,7 @@
 # Learn more: http://github.com/javan/whenever
 
 every 1.day, :at => '1:30 am' do
-  rake "RAILS_ENV=production db:dump"
+  rake "db:dump"
 end
 
 every 1.day, :at => '2:30 am' do
