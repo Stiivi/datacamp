22aa58129f7ea36b0e5a7b6614042d4dc7d2b570
Michal Olah <olahmichal@gmail.com>
2011-07-25 12:01:33 +0200

    adding delayed_job restart to production capistrano script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,3 +4,10 @@ set :rails_env, "production"
 # update crontab
 set :whenever_command, "bundle exec whenever"
 require "whenever/capistrano"
+
+require "delayed/recipes"
+set :rails_env, "production" #added for delayed job
+# Delayed Job
+after "deploy:stop",    "delayed_job:stop"
+after "deploy:start",   "delayed_job:start"
+after "deploy:restart", "delayed_job:restart"
