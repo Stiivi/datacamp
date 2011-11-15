929a09504535acfa43eb0e3f027fac9bd849512c
Michal Olah <m1k3@Michals-MacBook-Pro.local>
2011-11-15 16:36:47 +0100

    adding a detail view to the activities

--- features/activity_view.feature
+++ features/activity_view.feature
@@ -9,3 +9,9 @@ Feature: Activity overview
     And some recent activity exists
     When I display the recent activities
     Then I should see all of the recent activity that is in the database
+    
+  Scenario: Display a single activity record
+    Given I am a new, authenticated user "test" with password "password"
+    And some recent activity exists
+    When I display the first activity
+    Then I should see more information about the first activity
--- features/step_definitions/activity_view_steps.rb
+++ features/step_definitions/activity_view_steps.rb
@@ -12,3 +12,16 @@ Then /^I should see all of the recent activity that is in the database$/ do
   page.should have_content('something')
   page.should have_content(User.first.name)
 end
+
+When /^I display the first activity$/ do
+  And %{I display the recent activities}
+  click_link('View')
+end
+
+Then /^I should see more information about the first activity$/ do
+  activity = Change.first
+  page.should have_content(activity.dataset_description_identifier)
+  page.should have_content(activity.changed_field)
+  page.should have_content(activity.value)
+  page.should have_content(activity.user_name)
+end
