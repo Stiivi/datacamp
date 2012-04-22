938aeeb316722f70fd2c2cce1ec87ec159cfa3e4
Michal Olah <olahmichal@gmail.com>
2012-04-22 22:38:51 +0200

    ammend

--- lib/etl/lawyer_extraction.rb
+++ lib/etl/lawyer_extraction.rb
@@ -5,26 +5,26 @@ require 'fileutils'
 module Etl
   class LawyerExtraction
     attr_reader :url
-    
+
     def initialize(url, reset_url = nil, cookie = nil, parent_url = nil, filter = nil)
       @url, @reset_url, @cookie, @parent_url, @filter = url, reset_url, cookie, parent_url, filter
     end
-    
+
     def is_acceptable?(document)
       document.xpath("//div[@class='section']/table[@class='filter']").present?
     end
-    
+
     def download
       if @parent_url.present? && @cookie.present? && @reset_url
-        Typhoeus::Request.get(@reset_url, headers: {'Cookie' => @cookie}, disable_ssl_peer_verification: true))
-        Typhoeus::Request.get(@parent_url, headers: {'Cookie' => @cookie}, disable_ssl_peer_verification: true))
+        Typhoeus::Request.get(@reset_url, headers: {'Cookie' => @cookie}, disable_ssl_peer_verification: true)
+        Typhoeus::Request.get(@parent_url, headers: {'Cookie' => @cookie}, disable_ssl_peer_verification: true)
       end
-      Nokogiri::HTML( Typhoeus::Request.get(@url, headers: {'Cookie' => @cookie}, disable_ssl_peer_verification: true)).body )
+      Nokogiri::HTML( Typhoeus::Request.get(@url, headers: {'Cookie' => @cookie}, disable_ssl_peer_verification: true).body )
     end
-    
+
     def digest(doc)
       lawyer_table = doc.xpath("//div[@class='section']/table[@class='filter']").first
-      
+
       original_name = lawyer_table.xpath('./tr[1]/td[2]').inner_text.strip
       match_data = original_name.match(/(?<last_name>[^\s]+)\s+(?<first_name>[^\s]+)(\s+(?<title>[^\s]+))*/)
       sak_id = (@url.match(/\d+/)[0].to_i rescue nil)
@@ -48,7 +48,7 @@ module Etl
         :is_part_of_import => true
       }
     end
-    
+
     def perform
       document = download
       if is_acceptable?(document)
@@ -56,12 +56,12 @@ module Etl
         save(lawyer_hash)
       end
     end
-    
+
     def save(lawyer_hash)
-      lawyer = Kernel::DsLawyer.find_or_initialize_by_sak_id(lawyer_hash[:sak_id]) 
+      lawyer = Kernel::DsLawyer.find_or_initialize_by_sak_id(lawyer_hash[:sak_id])
       lawyer.update_attributes!(lawyer_hash)
     end
-    
+
     def get_downloads
       id = 0
       downloads = []
@@ -74,16 +74,16 @@ module Etl
       end while doc.xpath("//div[@class='buttonbar']/table//tr[1]//td[@style='opacity: 0.5']").inner_html.match(/but_arrow_right.gif/).blank?
       downloads.flatten
     end
-    
+
     def get_ids_from_downloads
       get_downloads.map{ |d| (d.url.match(/\d+/)[0].to_i rescue nil) }
     end
-    
+
     def parse_for_links(doc, reset_url, cookie, parent_url, filter)
       doc.xpath("//div[@class='result']/table//a").map do |link|
         Etl::LawyerExtraction.new("https://www.sak.sk/#{link.attributes['href'].value.match(/'(.*)'/)[1]}", reset_url, cookie, parent_url, filter)
       end
     end
-    
+
   end
 end
