7d8e2c9435ce24e3419d3e60c6a3c45ae94191b9
Michal Olah <olahmichal@gmail.com>
2012-10-17 08:46:57 +0200

    fix for http://bugtracker.fair-play.sk/issues/141

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -21,16 +21,15 @@
 
 class MainController < ApplicationController
   layout "frontend_main"
-  
+
   before_filter :login_required, :except => [:index, :locale]
-  
+
   def index
-    return redirect_to datasets_path, notice: flash[:notice] if logged_in?
     if index_page = Page.find_by_page_name("index")
       redirect_to page_path(index_page)
     end
   end
-  
+
   def locale
     target = (request.referer && !request.referer.empty?) ? request.referer : root_path
     I18n.locale = params[:locale] if params[:locale].present?
