0702f15aa014409612e174293881b80e3347ad8a
Michal Olah <olahmichal@gmail.com>
2014-02-21 16:37:50 +0100

    fixed smtp settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,14 @@
 if Rails.env.production? && Datacamp::Config.get(:gmail_user).present? && Datacamp::Config.get(:gmail_password).present?
-  ActionMailer::Base.delivery_method = :smtp  
+  require 'tlsmail'
+  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :enable_starttls_auto => true,
     :tls => true,
     :address => 'smtp.gmail.com',
     :port => "587",
-    :authentication => :plain,                                                                                                                     
+    :authentication => :plain,
     :domain => Datacamp::Config.get(:gmail_domain),
     :user_name => Datacamp::Config.get(:gmail_user),
     :password => Datacamp::Config.get(:gmail_password)
