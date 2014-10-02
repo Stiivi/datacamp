660737605962c79917139a7da60258518e99a98e
Ondrej Proksa <ondrej.proksa@gmail.com>
2014-10-02 10:38:01 +0200

    Fix system variable cache exception (bug #366)

--- app/models/system_variable.rb
+++ app/models/system_variable.rb
@@ -5,7 +5,12 @@ class SystemVariable < ActiveRecord::Base
   
   def self.get(name, default = nil)
     @variables_cache ||= self.all
-    variable = @variables_cache.detect { |variable| variable.name == name.to_s }
+    begin
+      variable = @variables_cache.detect { |variable| variable.name == name.to_s }
+    rescue
+      variable = find_by_name(name)
+      @variables_cache = self.all
+    end
     value = variable ? variable.value : default
     value = false if value == "0"
     value
