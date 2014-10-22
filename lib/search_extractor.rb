4b21441cbc2b02c6d5ca9338352466112f13b5c0
Ondrej Proksa <ondrej.proksa@gmail.com>
2014-10-22 22:29:46 +0200

    Analyze logs fix col_sep (support #363)

--- lib/search_extractor.rb
+++ lib/search_extractor.rb
@@ -2,7 +2,7 @@
 class SearchExtractor
 
   def self.extract
-    CSV.open("data/searches_#{Date.today.strftime('%Y_%m_%d')}.csv", "wb") do |csv|
+    CSV.open("data/searches_#{Date.today.strftime('%Y_%m_%d')}.csv", "wb", { col_sep: ';' }) do |csv|
       csv << ['ID', 'created at', 'query', 'type', 'scope', 'object']
       Search.find_each do |search|
         csv << [search.id, search.created_at.strftime('%Y-%m-%d %H:%M'), search.query_string,
