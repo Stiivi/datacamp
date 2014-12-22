8860346e016f886da8e2c922100952dbd5a92a37
Ondrej Proksa <ondrej.proksa@gmail.com>
2014-12-22 13:04:06 +0100

    Fix visibility in hidden records after search (hotfix)

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -54,6 +54,7 @@ class SearchesController < ApplicationController
       dds.where(:is_active => true).each do |dataset_description|
         begin
         dataset_results = dataset_description.dataset.dataset_record_class.search @search.query_string, :limit => 5
+        dataset_results = dataset_results.select{|r| r.record_status == DatastoreManager.record_statuses[2]}
         @results[dataset_category] ||= {} and @results[dataset_category].merge!({dataset_description=>dataset_results}) if dataset_results.present?
         rescue
         end
