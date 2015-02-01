736b888858c48cedd16af6f8caa18765876adc24
Ondrej Proksa <ondrej.proksa@gmail.com>
2015-02-01 18:37:00 +0100

    Change float attributes to decimal in v2 tables (hotfix)

--- db/schema_data.rb
+++ db/schema_data.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150115104528) do
+ActiveRecord::Schema.define(:version => 20150201162746) do
 
   create_table "dc_relations", :primary_key => "_record_id", :force => true do |t|
     t.integer "relatable_left_id"
@@ -364,79 +364,88 @@ ActiveRecord::Schema.define(:version => 20150115104528) do
   end
 
   create_table "ds_procurement_v2_notices", :primary_key => "_record_id", :force => true do |t|
-    t.integer "document_id",                         :null => false
-    t.string  "document_url",                        :null => false
-    t.string  "procurement_code",                    :null => false
-    t.integer "bulletin_code",                       :null => false
-    t.integer "year",                                :null => false
-    t.string  "procurement_type",                    :null => false
-    t.date    "published_on",                        :null => false
-    t.string  "customer_name"
-    t.string  "customer_organisation_code"
-    t.integer "customer_organisation_id"
-    t.string  "customer_regis_name"
-    t.string  "customer_address"
-    t.string  "customer_place"
-    t.string  "customer_zip"
-    t.string  "customer_country"
-    t.string  "customer_contact_persons"
-    t.string  "customer_phone"
-    t.string  "customer_mobile"
-    t.string  "customer_email"
-    t.string  "customer_fax"
-    t.string  "customer_type"
-    t.boolean "customer_purchase_for_others"
-    t.text    "customer_main_activity"
-    t.text    "project_name"
-    t.string  "project_type"
-    t.string  "project_category"
-    t.text    "place_of_performance"
-    t.string  "nuts_code"
-    t.text    "project_description"
-    t.boolean "general_contract"
-    t.text    "dictionary_main_subjects"
-    t.text    "dictionary_additional_subjects"
-    t.boolean "gpa_agreement"
-    t.string  "procedure_type"
-    t.text    "procedure_offers_criteria"
-    t.boolean "procedure_use_auction"
-    t.string  "previous_notification1_number"
-    t.date    "previous_notification1_published_on"
-    t.string  "previous_notification2_number"
-    t.date    "previous_notification2_published_on"
-    t.string  "previous_notification3_number"
-    t.date    "previous_notification3_published_on"
-    t.text    "contract_name"
-    t.date    "contract_date"
-    t.integer "offers_total_count"
-    t.integer "offers_online_count"
-    t.integer "offers_excluded_count"
-    t.integer "supplier_index"
-    t.string  "supplier_name"
-    t.string  "supplier_organisation_code"
-    t.integer "supplier_organisation_id"
-    t.string  "supplier_regis_name"
-    t.string  "supplier_address"
-    t.string  "supplier_place"
-    t.string  "supplier_zip"
-    t.string  "supplier_country"
-    t.string  "supplier_phone"
-    t.string  "supplier_mobile"
-    t.string  "supplier_email"
-    t.string  "supplier_fax"
-    t.string  "procurement_currency"
-    t.float   "final_price"
-    t.boolean "final_price_range"
-    t.float   "final_price_min"
-    t.float   "final_price_max"
-    t.boolean "final_price_vat_included"
-    t.float   "final_price_vat_rate"
-    t.float   "draft_price"
-    t.boolean "draft_price_vat_included"
-    t.float   "draft_price_vat_rate"
-    t.boolean "procurement_subcontracted"
-    t.boolean "procurement_euro_found"
-    t.text    "evaluation_committee"
+    t.integer  "document_id",                                                        :null => false
+    t.string   "document_url",                                                       :null => false
+    t.string   "procurement_code",                                                   :null => false
+    t.integer  "bulletin_code",                                                      :null => false
+    t.integer  "year",                                                               :null => false
+    t.string   "procurement_type",                                                   :null => false
+    t.date     "published_on",                                                       :null => false
+    t.string   "customer_name"
+    t.string   "customer_organisation_code"
+    t.integer  "customer_organisation_id"
+    t.string   "customer_regis_name"
+    t.string   "customer_address"
+    t.string   "customer_place"
+    t.string   "customer_zip"
+    t.string   "customer_country"
+    t.string   "customer_contact_persons"
+    t.string   "customer_phone"
+    t.string   "customer_mobile"
+    t.string   "customer_email"
+    t.string   "customer_fax"
+    t.string   "customer_type"
+    t.boolean  "customer_purchase_for_others"
+    t.text     "customer_main_activity"
+    t.text     "project_name"
+    t.string   "project_type"
+    t.string   "project_category"
+    t.text     "place_of_performance"
+    t.string   "nuts_code"
+    t.text     "project_description"
+    t.boolean  "general_contract"
+    t.text     "dictionary_main_subjects"
+    t.text     "dictionary_additional_subjects"
+    t.boolean  "gpa_agreement"
+    t.string   "procedure_type"
+    t.text     "procedure_offers_criteria"
+    t.boolean  "procedure_use_auction"
+    t.string   "previous_notification1_number"
+    t.date     "previous_notification1_published_on"
+    t.string   "previous_notification2_number"
+    t.date     "previous_notification2_published_on"
+    t.string   "previous_notification3_number"
+    t.date     "previous_notification3_published_on"
+    t.text     "contract_name"
+    t.date     "contract_date"
+    t.integer  "offers_total_count"
+    t.integer  "offers_online_count"
+    t.integer  "offers_excluded_count"
+    t.integer  "supplier_index"
+    t.string   "supplier_name"
+    t.string   "supplier_organisation_code"
+    t.integer  "supplier_organisation_id"
+    t.string   "supplier_regis_name"
+    t.string   "supplier_address"
+    t.string   "supplier_place"
+    t.string   "supplier_zip"
+    t.string   "supplier_country"
+    t.string   "supplier_phone"
+    t.string   "supplier_mobile"
+    t.string   "supplier_email"
+    t.string   "supplier_fax"
+    t.string   "procurement_currency"
+    t.decimal  "final_price",                         :precision => 16, :scale => 2
+    t.boolean  "final_price_range"
+    t.decimal  "final_price_min",                     :precision => 16, :scale => 2
+    t.decimal  "final_price_max",                     :precision => 16, :scale => 2
+    t.boolean  "final_price_vat_included"
+    t.decimal  "final_price_vat_rate",                :precision => 16, :scale => 2
+    t.decimal  "draft_price",                         :precision => 16, :scale => 2
+    t.boolean  "draft_price_vat_included"
+    t.decimal  "draft_price_vat_rate",                :precision => 16, :scale => 2
+    t.boolean  "procurement_subcontracted"
+    t.boolean  "procurement_euro_found"
+    t.text     "evaluation_committee"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "created_by"
+    t.string   "updated_by"
+    t.string   "record_status"
+    t.string   "quality_status"
+    t.integer  "batch_id"
+    t.date     "validity_date"
+    t.boolean  "is_hidden"
   end
 
   add_index "ds_procurement_v2_notices", ["customer_organisation_id"], :name => "index_ds_procurement_v2_notices_on_customer_organisation_id"
@@ -445,13 +454,13 @@ ActiveRecord::Schema.define(:version => 20150115104528) do
   add_index "ds_procurement_v2_notices", ["supplier_organisation_id"], :name => "index_ds_procurement_v2_notices_on_supplier_organisation_id"
 
   create_table "ds_procurement_v2_performances", :primary_key => "_record_id", :force => true do |t|
-    t.integer "document_id",                   :null => false
-    t.string  "document_url",                  :null => false
-    t.string  "procurement_code",              :null => false
-    t.integer "bulletin_code",                 :null => false
-    t.integer "year",                          :null => false
-    t.string  "procurement_type",              :null => false
-    t.date    "published_on",                  :null => false
+    t.integer "document_id",                                                  :null => false
+    t.string  "document_url",                                                 :null => false
+    t.string  "procurement_code",                                             :null => false
+    t.integer "bulletin_code",                                                :null => false
+    t.integer "year",                                                         :null => false
+    t.string  "procurement_type",                                             :null => false
+    t.date    "published_on",                                                 :null => false
     t.string  "project_type"
     t.string  "customer_name"
     t.string  "customer_organisation_code"
@@ -473,12 +482,12 @@ ActiveRecord::Schema.define(:version => 20150115104528) do
     t.date    "subcontract_date"
     t.text    "subcontract_reason"
     t.string  "procurement_currency"
-    t.float   "final_price"
+    t.decimal "final_price",                   :precision => 16, :scale => 2
     t.boolean "final_price_vat_included"
-    t.float   "final_price_vat_rate"
-    t.float   "draft_price"
+    t.decimal "final_price_vat_rate",          :precision => 16, :scale => 2
+    t.decimal "draft_price",                   :precision => 16, :scale => 2
     t.boolean "draft_price_vat_included"
-    t.float   "draft_price_vat_rate"
+    t.decimal "draft_price_vat_rate",          :precision => 16, :scale => 2
     t.text    "price_difference_reason"
     t.date    "start_on"
     t.date    "end_on"
@@ -493,6 +502,19 @@ ActiveRecord::Schema.define(:version => 20150115104528) do
 
   add_index "ds_procurement_v2_performances", ["customer_organisation_id"], :name => "index_ds_procurement_v2_performances_on_customer_organisation_id"
   add_index "ds_procurement_v2_performances", ["document_id"], :name => "index_ds_procurement_v2_performances_on_document_id"
+
+  create_table "ds_projects", :primary_key => "_record_id", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "created_by"
+    t.string   "updated_by"
+    t.string   "record_status"
+    t.string   "quality_status"
+    t.integer  "batch_id"
+    t.date     "validity_date"
+    t.boolean  "is_hidden"
+    t.integer  "name"
+  end
 
   create_table "ds_relation_testings", :primary_key => "_record_id", :force => true do |t|
     t.string   "created_by"
