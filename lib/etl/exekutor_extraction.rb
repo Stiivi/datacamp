be2221aa86b580467b34e84279b283816384fb86
Miroslav Hettes <mirrec@gmail.com>
2015-06-15 16:41:38 +0200

    Refactor executor extractron and update tests

--- lib/etl/exekutor_extraction.rb
+++ lib/etl/exekutor_extraction.rb
@@ -2,13 +2,24 @@
 
 module Etl
   class ExekutorExtraction
+    attr_reader :downloader, :document_url, :storage
 
-    def document_url
-      "http://www.ske.sk/zoznam-exekutorov/"
+    def initialize(options = {})
+      @downloader = options.fetch(:downloader) { Etl::Executor::Downloader }
+      @storage = options.fetch(:storage) { Kernel::DsExecutor }
+      @document_url = options.fetch(:document_url) { "http://www.ske.sk/zoznam-exekutorov/" }
     end
 
-    def download
-      Nokogiri::HTML(Typhoeus::Request.get(document_url).body)
+    def perform
+      store_executors
+    end
+
+    def store_executors
+      if elements_to_parse_count > 0
+        storage.suspend_all
+        active_executor_ids = page.executors.map { |executor_hash| storage.store_executor(executor_hash).id }
+        storage.publish_with_ids(active_executor_ids)
+      end
     end
 
     def self.update_last_run_time
@@ -16,54 +27,70 @@ module Etl
       DatasetDescription.find_by_identifier('executors').update_attribute(:data_updated_at, Time.zone.now)
     end
 
-    def is_acceptable?(document)
-      document.xpath("//table[@id='executors']").present?
-    end
-
     def elements_to_parse_count
-      download.xpath("//table[@id='executors']//tr[@class='listtr']").count - 2 # ignore first and last
+      page.executors_count
     end
 
-    def digest(doc)
-      doc.xpath("//table[@id='executors']//tr[@class='listtr']")[1..-2].map do |list_item_tr|
-        td_1 = list_item_tr.xpath(".//td[1]")
-        td_2 = list_item_tr.xpath(".//td[2]")
-        address = (td_2.inner_html.match(/<strong>Adresa: <\/strong>(?<address>.*?)<br>/)[:address].strip rescue nil)
-        if address
-          street = address.split(',').first
-          city = address.split(',').last.match(/(?<zip>\d+ \d+|\d+)\s?(?<city>[^\d]+\d?)/)[:city] if address.split(',').last.match(/(?<zip>\d+ \d+|\d+)\s?(?<city>[^\d]+\d?)/).present?
-          zip = address.split(',').last.match(/ (?<zip>\d+ \d+|\d+)/)[:zip] if address.split(',').last.match(/ (?<zip>\d+ \d+|\d+)/).present?
+    def page
+      @page ||= Etl::Executor::ListingPage.new(load_page)
+    end
+
+    private
+
+    def load_page
+      downloader.download(document_url)
+    end
+  end
+
+  module Executor
+    class Downloader
+      def self.download(url)
+        Nokogiri::HTML(Typhoeus::Request.get(url).body)
+      end
+    end
+
+    class ListingPage
+      attr_reader :document
+
+      def initialize(document)
+        @document = document
+      end
+
+      def executors_count
+        executors.count
+      end
+
+      def executors
+        return [] unless acceptable?
+
+        @executors ||= begin
+          document.xpath("//table[@id='executors']//tr[@class='listtr']")[1..-2].map do |list_item_tr|
+            td_1 = list_item_tr.xpath(".//td[1]")
+            td_2 = list_item_tr.xpath(".//td[2]")
+            address = (td_2.inner_html.match(/<strong>Adresa: <\/strong>(?<address>.*?)<br>/)[:address].strip rescue nil)
+            if address
+              street = address.split(',').first
+              city = address.split(',').last.match(/(?<zip>\d+ \d+|\d+)\s?(?<city>[^\d]+\d?)/)[:city] if address.split(',').last.match(/(?<zip>\d+ \d+|\d+)\s?(?<city>[^\d]+\d?)/).present?
+              zip = address.split(',').last.match(/ (?<zip>\d+ \d+|\d+)/)[:zip] if address.split(',').last.match(/ (?<zip>\d+ \d+|\d+)/).present?
+            end
+            {
+                :name => td_1.inner_text.strip,
+                :street => street,
+                :zip => zip,
+                :city => city,
+                :telephone => (td_2.inner_html.match(/<strong>Tel.: <\/strong>(?<tel>.*?)<br>/)[:tel].strip rescue nil),
+                :fax => (td_2.inner_html.match(/<strong>Fax: <\/strong>(?<fax>.*?)<br>/)[:fax].strip rescue nil),
+                :email => (td_2.inner_html.match(/<strong>E-mail: <\/strong>(?<email>[^ @]+@[^ @]+\.[a-zA-Z]{2,4})/)[:email].strip rescue nil)
+            }
+          end
         end
-        {
-          :name => td_1.inner_text.strip,
-          :street => street,
-          :zip => zip,
-          :city => city,
-          :telephone => (td_2.inner_html.match(/<strong>Tel.: <\/strong>(?<tel>.*?)<br>/)[:tel].strip rescue nil),
-          :fax => (td_2.inner_html.match(/<strong>Fax: <\/strong>(?<fax>.*?)<br>/)[:fax].strip rescue nil),
-          :email => (td_2.inner_html.match(/<strong>E-mail: <\/strong>(?<email>[^ @]+@[^ @]+\.[a-zA-Z]{2,4})/)[:email].strip rescue nil)
-        }
+      end
+
+      private
+
+      def acceptable?
+        document.xpath("//table[@id='executors']").present?
       end
     end
-
-
-    def save(executors_hash)
-      Kernel::DsExecutor.update_all(record_status: 'suspended')
-      active_executor_ids = executors_hash.map do |executor_hash|
-        executor = Kernel::DsExecutor.find_or_initialize_by_name_and_city(executor_hash[:name], executor_hash[:city])
-        executor.update_attributes(executor_hash)
-        executor.id
-      end
-      Kernel::DsExecutor.where(_record_id: active_executor_ids).update_all(record_status: 'published')
-    end
-
-    def perform
-      document = download
-      if is_acceptable?(document)
-        executors_hash = digest(document)
-        save(executors_hash)
-      end
-    end
-
   end
 end
--- spec/lib/etl/exekutor_extraction_spec.rb
+++ spec/lib/etl/exekutor_extraction_spec.rb
@@ -1,40 +1,99 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
-
-class Kernel::DsExecutor; end
+require 'etl/exekutor_extraction'
 
 describe Etl::ExekutorExtraction do
-  before :each do
-    @extractor = Etl::ExekutorExtraction.new
-  end
-  
-  it 'should have a perform method that downloads, parses and saves to the db' do
-    VCR.use_cassette('etl/executor_extraction/executors') do
-      @extractor.should_receive(:save)
-      @extractor.perform
-    end
-  end
-  
-  describe 'parsing' do
-    it 'should test and accept valid documents' do
-      VCR.use_cassette('etl/executor_extraction/executors') do
-        document = @extractor.download
-        @extractor.is_acceptable?(document).should be_true
+  let!(:tony_executor) { Kernel::DsExecutor.create!(name: 'Tony', record_status: 'published') }
+
+  describe '#perform' do
+    it 'saves extracted executors to database and mark old records as suspended' do
+      VCR.use_cassette('etl/executor_extraction/executors_letter_i') do
+        extration = Etl::ExekutorExtraction.new(
+            document_url: 'http://www.ske.sk/zoznam-exekutorov/?f=I'
+        )
+        extration.perform
+
+        tony_executor.reload.record_status.should eq 'suspended'
+        Kernel::DsExecutor.count.should be > 2
+        Kernel::DsExecutor.find_by_name!('JUDr. Ingr Jozef').record_status.should eq 'published'
       end
     end
-    
-    it 'should get information from a document' do
+
+    it 'does not change anything in storage if page does not have any executors' do
+      VCR.use_cassette('etl/executor_extraction/invalid_site') do
+        extration = Etl::ExekutorExtraction.new(
+            document_url: 'http://www.ske.sk/category/aktuality/'
+        )
+        extration.perform
+      end
+
+      tony_executor.reload.record_status.should eq 'published'
+      Kernel::DsExecutor.count.should eq 1
+    end
+  end
+end
+
+describe Etl::Executor::Downloader do
+
+  describe '.download' do
+    it 'downloads given url to document' do
       VCR.use_cassette('etl/executor_extraction/executors') do
-        extractor = Etl::ExekutorExtraction.new
-        document = extractor.download
-        executors = extractor.digest(document)
-        executors.length.should be > 300
-        executors.first.should == {:name=>"JUDr. Ádám Atila", :street => 'Čajakova 5', :zip => '04001', :city => 'Košice', :telephone=>"+421 55/6424552", :fax=>"+421 55/6424552", :email=>"atila.adam@ske.sk"}
-        executors.last.should == {:name=>"JUDr. Žabková Žofia", :street => 'Námestie Matice Slovenskej 6', :zip => '96501', :city => 'Žiar nad Hronom', :telephone=>"0948/011 261", :fax=>"-", :email=>"zofia.zabkova@ske.sk"}
-        executor = executors.find{ |executor| executor[:name] == "JUDr. Stodolová Soňa" }
-        executor.should == {:name=>"JUDr. Stodolová Soňa", :street=>"Kmeťková 25", :zip=>"94901", :city=>"Nitra", :telephone=>"037 6514444", :fax=>"037 6514444", :email=>"sona.stodolova@ske.sk"}
+        document = Etl::Executor::Downloader.download('http://www.ske.sk/zoznam-exekutorov/')
+        document.should respond_to(:xpath)
       end
     end
-    
+  end
+end
+
+describe Etl::Executor::ListingPage do
+
+  describe '#executors' do
+    it 'returns executors array of hashes' do
+      executors_document = VCR.use_cassette('etl/executor_extraction/executors') do
+         Etl::Executor::Downloader.download('http://www.ske.sk/zoznam-exekutorov/')
+      end
+
+      executors = Etl::Executor::ListingPage.new(executors_document).executors
+      executors.length.should be > 300
+
+      executor = executors.find{ |executor| executor[:name] == 'JUDr. Ingr Jozef' }
+      executor.should == {
+          name: 'JUDr. Ingr Jozef',
+          street: 'Matejkova 13',
+          zip: '84105',
+          city: 'Bratislava',
+          telephone: '02/65411505',
+          fax: '02/65411505',
+          email: 'jozef.ingr@ske.sk',
+      }
+
+      executors.each do |executor|
+        executor[:name].should match_regex /^[\p{Letter}\. ,\(\)]+$/
+        executor[:street].should match_regex /^[\p{Letter}\. ,0-9\\\/\-]+$/
+        if executor[:zip]
+          executor[:zip].should match_regex /^[0-9 ]+$/
+        end
+        if executor[:city]
+          executor[:city].should match_regex /^[\p{Letter} 0-9]+$/
+        end
+        executor[:telephone].should match_regex /^[0-9 ,\+\-\\\/]+$/
+        executor[:fax].should match_regex /^[0-9 ,\+\-\\\/]+$/
+        executor[:email].should match_regex /^[a-z0-9\.@]+$/
+      end
+
+      # WATCH OUT FOR !
+      executor = executors.find { |executor| executor[:name] == 'Mgr. Makita Ján' }
+      executor[:telephone].should eq '02 62525594, 95, 96'
+      executor[:fax].should eq '-'
+    end
+
+    it 'returns empty array if page is not acceptable' do
+      executors_document = VCR.use_cassette('etl/executor_extraction/invalid_site') do
+        Etl::Executor::Downloader.download('http://www.ske.sk/category/aktuality/')
+      end
+
+      executors = Etl::Executor::ListingPage.new(executors_document).executors
+      executors.should eq []
+    end
   end
 end
